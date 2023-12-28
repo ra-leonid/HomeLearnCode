90e36b1d42afc4ad832ecc84028e402fdb0f2bef
Leonid Rastorguev <rastorguyevl@list.ru>
2023-12-28 20:52:48 +0300

    https://developer.alexanderklimov.ru/android/views/recyclerview-kot.php

--- mobile/src/main/java/com/example/dasha/homelearncode/Exercises.kt
+++ mobile/src/main/java/com/example/dasha/homelearncode/Exercises.kt
@@ -2,11 +2,21 @@ package com.example.dasha.homelearncode
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class Exercises : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercises)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList())
     }
-}
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("$i element") }
+        return data
+    }}
